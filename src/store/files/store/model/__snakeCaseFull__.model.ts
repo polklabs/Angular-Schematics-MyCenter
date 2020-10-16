0714c9f31fe469c6e10ff72040a1a9bda3d926e3
Polk <Apolk@TRI.NET>
2020-10-16 14:04:03 -0700

    Added NoNarrow type to model template

--- src/store/files/store/model/__snakeCaseFull__.model.ts
+++ src/store/files/store/model/__snakeCaseFull__.model.ts
@@ -1,6 +1,7 @@
-<% if(storeType === 'entity') { %>import { <%= fullNameUpper %>Base } from '../generated-model/<%= snakeCaseFull %>-base.model'
+<% if(storeType === 'entity') { %>import { NoNarrow } from 'src/app/shared/model/no-narrow.type';
+import { <%= fullNameUpper %>Base } from '../generated-model/<%= snakeCaseFull %>-base.model';
 
-<% } %>export class <%= fullNameUpper %> <% if(storeType === 'entity') { %>extends <%= fullNameUpper %>Base {
+<% } %>export class <%= fullNameUpper %> <% if(storeType === 'entity') { %>extends <%= fullNameUpper %>Base implements NoNarrow<<%= fullNameUpper %>, <%= fullNameUpper %>Base> {
     $id: string;
     $type: string;<% } else { %>{<% } %>
 }
